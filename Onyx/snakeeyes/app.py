9f550bc2cb6277e33b526a1cacf1d7f030090e96
Brian <41885974+bwalsh1191@users.noreply.github.com>
2019-01-01 19:40:45 -0500

    initial commit

---
+++ Onyx/snakeeyes/app.py
@@ -0,0 +1,20 @@
+from flask import Flask
+
+#import this to use in the program. this allows us to use the 'page' template in the file for this
+from snakeeyes.blueprints.page import page
+
+
+def create_app():
+    """
+    Create a Flask application using the app factory pattern.
+
+    :return: Flask app
+    """
+    app = Flask(__name__, instance_relative_config=True)
+
+    app.config.from_object('config.settings')
+    app.config.from_pyfile('settings.py', silent=True)
+
+    app.register_blueprint(page)
+
+    return app
---
+++ Onyx/snakeeyes/blueprints/page/views.py
@@ -0,0 +1,108 @@
+
+#instead of importing flask, we import blueprint
+from flask import Blueprint, render_template
+import json
+import html.parser as htmlparser
+from datetime import datetime
+import requests
+import pandas as pd
+import matplotlib.pyplot as plt
+
+#create a blueprint with a name of 'page'
+page = Blueprint('page', __name__, template_folder='templates')
+
+#instead of doing app.route, we attach it to the blueprint route
+@page.route('/')
+def home():
+
+    #renders a html template instead 
+    return render_template('page/home.html')
+
+
+@page.route('/dash')
+def dash():
+
+    API_KEY = '4HNDQOUQ2A1G90RW'
+    symbol = 'AAPL'
+    company = "Apple (AAPL)"
+    
+    #works but not for testing. Dont want to get rate limited
+    '''    
+    #use this one for testing
+    url1 = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=' + symbol + '&outputsize=compact&datatype=csv&apikey=' + API_KEY
+    url2 = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=' + symbol + '&interval=5min&datatype=csv&apikey=' + API_KEY
+    df_daily = pd.read_csv(url1, index_col="timestamp", parse_dates = True, na_values = ' ')
+    df_current = pd.read_csv(url2, index_col="timestamp", parse_dates = True, na_values = ' ')
+
+    df_daily['close'] = df_daily['adjusted_close']
+    df_daily.index.names = ['date']
+    df_current.index.names = ['date']
+
+    
+
+
+    current_price = '${:,.2f}'.format(df_current.iloc[0]['open'])
+    high_price = '${:,.2f}'.format(df_daily.iloc[0]['high'])
+    low_price = '${:,.2f}'.format(df_daily.iloc[0]['low'])
+    volume = "{:,}".format(int(df_daily.iloc[0]['volume']))
+
+    '''
+
+    #use this for rate limiting testing purposes
+    current_price = "$100.94"
+    high_price = "$400.43"
+    low_price = "$400.56"
+    volume = "300,001"
+    daily_change = "$2.43"
+
+    stock_info = [company,current_price, daily_change,high_price, low_price, volume]
+
+
+
+    '''
+    parser = htmlparser.HTMLParser()
+    new_symbol = 'AAPl'
+    
+    url = 'https://api.stocktwits.com/api/2/streams/symbol/{}.json'
+
+    stockTwits = requests.get(url.format(new_symbol)).json()
+    stockTwits_data = []
+    new_sent = ''
+
+    for index in range (0,30):
+        
+        message = stockTwits['messages'][index]['body']
+        username = stockTwits['messages'][index]['user']['username']
+        sentiment = str(stockTwits['messages'][index]['entities']['sentiment'])
+
+        if(sentiment == "{'basic': 'Bullish'}"):
+            new_sent = 'Bullish'
+
+        elif(sentiment == "{'basic': 'Bearish'}"):
+            new_sent = 'Bearish'
+        else:
+            new_sent = ''
+
+        new_message= parser.unescape(message)
+        
+        stockTwitsData = {
+            'message' : new_message,
+            'username' : "username: " + username,
+            'sentiment' : new_sent,
+        }
+        
+        stockTwits_data.append(stockTwitsData)
+
+    '''
+    stockTwits_data = []
+
+    for x in range (0,30):
+        
+        stockTwitsData = {
+            'message' : "brian",
+            'username' : "username: ",
+            'sentiment' : "bullish",
+        }
+        
+        stockTwits_data.append(stockTwitsData)
+    return render_template('page/dash.1.html',stockTwits_data=stockTwits_data,stock_info=stock_info)
